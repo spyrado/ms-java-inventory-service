6335e55ef0ca14eb51ca6cf3bcb807ba695d6521
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2026-05-19 21:06:52 -0300

    feat: aplicando o padrao DLT para armazenar os topicos com falha

--- src/main/java/com/ecommerce/inventory_service/config/KafkaErrorHandlerConfig.java
+++ src/main/java/com/ecommerce/inventory_service/config/KafkaErrorHandlerConfig.java
@@ -2,6 +2,8 @@ package com.ecommerce.inventory_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
@@ -9,10 +11,14 @@ import org.springframework.util.backoff.FixedBackOff;
 public class KafkaErrorHandlerConfig {
 
   @Bean
-  public DefaultErrorHandler errorHandler() {
-    // tenta 3 vezes com intervalo de 2 segundos
-    return new DefaultErrorHandler(new FixedBackOff(2000L, 3));
-    //                                               ↑       ↑
-    //                                          intervalo  tentativas
+  public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+    // após 3 tentativas manda para a DLT
+    DeadLetterPublishingRecoverer recoverer =
+        new DeadLetterPublishingRecoverer(kafkaTemplate);
+
+    // 3 tentativas com 2 segundos de intervalo
+    FixedBackOff backOff = new FixedBackOff(2000L, 3);
+
+    return new DefaultErrorHandler(recoverer, backOff);
   }
 }
